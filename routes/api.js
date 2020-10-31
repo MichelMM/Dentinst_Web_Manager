7ce1b154355e1d9aea5069ad55852cad8453de0d
casillas-m <si715612@iteso.mx>
2020-10-31 17:58:58 -0600

    Added sendStatus to error resposes and documentation of patch endpoints

--- routes/api.js
+++ routes/api.js
@@ -22,7 +22,7 @@ router.get('/dentists',function(req,res){
             res.send(results);
         })
       }).catch(function(err){
-        res.send('ERROR')
+        res.send(`ERROR: ${err}`).sendStatus(400);
       });
 });
 
@@ -33,8 +33,58 @@ router.get('/dentist',function(req,res){
             res.send(results);
         })
       }).catch(function(err){
-        res.send('ERROR')
+        res.send(`ERROR: ${err}`).sendStatus(400);
       });
+});
+
+router.patch('/dentist',function(req,res){
+  updateMongo("Dentist",req.body.filter,req.body.data,req.body.many).then(function(collection){
+      collection.update(function(results){
+          res.send(results);
+      })
+    }).catch(function(err){
+      res.send(`ERROR: ${err}`).sendStatus(400);
+    });
+/**
+* @swagger 
+* /api/dentist: 
+*   patch: 
+*     summary: Update one or many "Dentist" documents
+*     requestBody:
+*       description: filter, data, [many]. If not filter, then filter={}
+*       required: true
+*       content:
+*         application/json:
+*           schema:
+*             type: object
+*             properties:
+*               filter:
+*                 type: object
+*               data:
+*                 type: object
+*               many:
+*                 type: boolean
+*           example: {"data":{"$set":{"Name":"Miguel Robertooo"}}}
+*     responses: 
+*       200: 
+*         description: Documents updated
+*         content:
+*           application/json:
+*             schema:
+*               type: object
+*               properties:
+*                 matchedCount:
+*                   type: number
+*                 modifiedCount:
+*                   type: number
+*       400:
+*         description: Update error
+*         content:
+*           text/plain:
+*             schema:
+*               type: string
+*               example: Error [*err*]
+*/
 });
 
 router.get('/patients',function(req,res){
@@ -44,7 +94,7 @@ router.get('/patients',function(req,res){
           res.send(results);
         })
       }).catch(function(err){
-        res.send('ERROR')
+        res.send(`ERROR: ${err}`).sendStatus(400);
       });
 });
 
@@ -55,10 +105,59 @@ router.get('/patient',function(req,res){
           res.send(results);
         })
       }).catch(function(err){
-        res.send('ERROR')
+        res.send(`ERROR: ${err}`).sendStatus(400);
       });
 });
 
+router.patch('/patient',function(req,res){
+  updateMongo("Patient",req.body.filter,req.body.data,req.body.many).then(function(collection){
+      collection.update(function(results){
+          res.send(results);
+      })
+    }).catch(function(err){
+      res.send(`ERROR: ${err}`).sendStatus(400);
+    });
+/**
+* @swagger 
+* /api/patient: 
+*   patch: 
+*     summary: Update one or many "Patient" documents
+*     requestBody:
+*       description: filter, data, [many]. If not filter, then filter={}
+*       required: true
+*       content:
+*         application/json:
+*           schema:
+*             type: object
+*             properties:
+*               filter:
+*                 type: object
+*               data:
+*                 type: object
+*               many:
+*                 type: boolean
+*           example: {"data":{"$set":{"Name":"Antonio Banderas"}}}
+*     responses: 
+*       200: 
+*         description: Documents updated
+*         content:
+*           application/json:
+*             schema:
+*               type: object
+*               properties:
+*                 matchedCount:
+*                   type: number
+*                 modifiedCount:
+*                   type: number
+*       400:
+*         description: Update error
+*         content:
+*           text/plain:
+*             schema:
+*               type: string
+*               example: Error [*err*]
+*/
+});
 
 router.get('/invoices',function(req,res){
     
@@ -67,7 +166,7 @@ router.get('/invoices',function(req,res){
             res.send(results);
         })
       }).catch(function(err){
-        res.send('ERROR')
+        res.send(`ERROR: ${err}`).sendStatus(400);
       });
 });
 
@@ -78,8 +177,58 @@ router.get('/invoice',function(req,res){
             res.send(results);
         })
       }).catch(function(err){
-        res.send('ERROR')
+        res.send(`ERROR: ${err}`).sendStatus(400);
       });
+});
+
+router.patch('/invoice',function(req,res){
+  updateMongo("Invoice",req.body.filter,req.body.data,req.body.many).then(function(collection){
+      collection.update(function(results){
+          res.send(results);
+      })
+    }).catch(function(err){
+      res.send(`ERROR: ${err}`).sendStatus(400);
+    });
+/**
+* @swagger 
+* /api/invoice: 
+*   patch: 
+*     summary: Update one or many "Invoice" documents
+*     requestBody:
+*       description: filter, data, [many]. If not filter, then filter={}
+*       required: true
+*       content:
+*         application/json:
+*           schema:
+*             type: object
+*             properties:
+*               filter:
+*                 type: object
+*               data:
+*                 type: object
+*               many:
+*                 type: boolean
+*           example: {"data":{"$set":{"Invoice_ID":10}}}
+*     responses: 
+*       200: 
+*         description: Documents updated
+*         content:
+*           application/json:
+*             schema:
+*               type: object
+*               properties:
+*                 matchedCount:
+*                   type: number
+*                 modifiedCount:
+*                   type: number
+*       400:
+*         description: Update error
+*         content:
+*           text/plain:
+*             schema:
+*               type: string
+*               example: Error [*err*]
+*/
 });
 
 router.get('/appointments',function(req,res){
@@ -89,7 +238,7 @@ router.get('/appointments',function(req,res){
           res.send(results);
         })
       }).catch(function(err){
-        res.send('ERROR')
+        res.send(`ERROR: ${err}`).sendStatus(400);
       });
 });
 
@@ -99,9 +248,58 @@ router.get('/appointment',function(req,res){
           res.send(results);
         })
       }).catch(function(err){
-        res.send('ERROR')
+        res.send(`ERROR: ${err}`).sendStatus(400);
       });
 });
 
+router.patch('/appointment',function(req,res){
+  updateMongo("Appointment",req.body.filter,req.body.data,req.body.many).then(function(collection){
+      collection.update(function(results){
+          res.send(results);
+      })
+    }).catch(function(err){
+      res.send(`ERROR: ${err}`).sendStatus(400);
+    });
+/**
+* @swagger 
+* /api/appointment: 
+*   patch: 
+*     summary: Update one or many "Appointment" documents
+*     requestBody:
+*       description: filter, data, [many]. If not filter, then filter={}
+*       required: true
+*       content:
+*         application/json:
+*           schema:
+*             type: object
+*             properties:
+*               filter:
+*                 type: object
+*               data:
+*                 type: object
+*               many:
+*                 type: boolean
+*           example: {"filter":{"Appointment_ID":{"$eq":2}},"data":{"$set":{"Dentist_ID":100}}}
+*     responses: 
+*       200: 
+*         description: Documents updated
+*         content:
+*           application/json:
+*             schema:
+*               type: object
+*               properties:
+*                 matchedCount:
+*                   type: number
+*                 modifiedCount:
+*                   type: number
+*       400:
+*         description: Update error
+*         content:
+*           text/plain:
+*             schema:
+*               type: string
+*               example: Error [*err*]
+*/
+});
 
 module.exports = router;
